c86fee95e038208f7d03edfb6be521d9c22ba24e
Janhavi3045 <janhavi3045@gmail.com>
2026-07-17 13:23:01 +0530

    Initial commit

---
+++ Lambda_Function/StartEC2_lambda_function.py
@@ -0,0 +1,16 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+
+INSTANCE_ID = "i-037283f126e78bdf9"
+
+def lambda_handler(event, context):
+
+    ec2.start_instances(
+        InstanceIds=[INSTANCE_ID]
+    )
+
+    return {
+        'statusCode':200,
+        'body':'EC2 started successfully'
+    }
